625b22dceaf8260ecf6bc4f00fd2f780d7b085c1
miraishinohara <miraiminato8@yahoo.co.jp>
2016-06-27 17:29:46 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,22 +1,10 @@
-function outPutFizzBuzz(){  
-    const FIZZBUZZ_MAX_COUNT = 101;
-    const OUTPUT_FIZZ ='Fizz';
-    const OUTPUT_BUZZ ='Buzz';
-
-    for (i = 1; i < FIZZBUZZ_MAX_COUNT; i++){
-        if(i%3== 0 && i%5== 0){
-            alert(OUTPUT_FIZZ + OUTPUT_BUZZ)
-
-        }else if(i%3== 0){
-            alert(OUTPUT_FIZZ);
-
-        }else if(i%5== 0){
-            alert(OUTPUT_BUZZ);
-
-        }else{
-            alert(i);
-
-        }
-
-    }
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0)  // iが3の倍数かつ5の倍数
+    document.write("Fizz,Buzz<br>");
+  else if (i % 3 == 0)  // iが3の倍数(かつ5の倍数でない)
+    document.write("Fizz<br>");
+  else if (i % 5 == 0)  // iが5の倍数(かつ3の倍数でない)
+    document.write("Buzz<br>");
+  else  // iが3の倍数でも5の倍数でもない
+    document.write(i + "<br>");
 }
